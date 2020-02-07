f4de375f2d363a0d50858fe7b7f137853989631d
Amit Kumar <44119580+Mr-Amit@users.noreply.github.com>
2020-02-07 15:05:26 +0530

    Brief on code

--- Stickers.py
+++ Stickers.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[39]:
-
-
+#imports
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date
 import os
-
-
-# In[40]:
-
 
 
 path = input("Enter the path of the picture : (enter cwd for current working directory)")
@@ -31,31 +25,21 @@ while not os.path.isfile(name):
     print("Picture Doesn't Exists!")
     name = input("Enter the valid name of the picture with the extension : ")
     
-    
 cwd = os.getcwd()
-
-
-# In[41]:
-
 
 src = cv2.imread(name)
 
-
+#OpenCV uses BGR color format
 img = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
 
-# plt.imshow(img)
-# plt.show()
-#cv2.imshow('image', img)
-
-face_detect = cv2.CascadeClassifier("C:\\Users\lenovo\Desktop\mlworkshop\haarcascade_frontalface_alt.xml")
+#Location of haarcascade_frontalface_alt.xml file in my PC
+face_detect = cv2.CascadeClassifier("C:\\Users\lenovo\Desktop\haarcascade_frontalface_alt.xml")
 faces = face_detect.detectMultiScale(img, 1.1, 2)
 
+#if there are no faces detected, then line 37 return a tuple instead of an array
 if type(faces) == type(np.array([1])):
+    #To access List functionalities
     faces = faces.tolist()
-
-
-# In[42]:
-
 
 cut = []
 
@@ -67,22 +51,17 @@ for face in faces:
     
     x, y, h, w = face
     
+    #Making the filename
     today = str(date.today()).replace("-","")
-    filename = "STK-" + today + "-WA" + "0"*(4 - len(str(faces.index(face)))) + str(faces.index(face)) + ".webp"#
-#    print(filename)
+    filename = "STK-" + today + "-WA" + "0"*(4 - len(str(faces.index(face)))) + str(faces.index(face)) + ".webp"
     
     cut.append(img[y : y + h, x : x + w, : ])
     im = cut[-1].copy()
     
     im2save = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-#     im.copy()
-    
-#     plt.axis("off")
-#     plt.imshow(im)
-    
-#     plt.show()
 
     _ = cv2.imwrite(filename, im2save)
-    
+
+#Back to working Directory    
 os.chdir(cwd)
 
